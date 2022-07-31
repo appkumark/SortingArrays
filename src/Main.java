f316d8faeddd72e5cfedf7e5a845fd2975de536e
appkumark <appk.kanubuddi@gmail.com>
2022-07-31 20:21:38 +0530

    Method commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+
+        Arrays.parallelSort(intArray);
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println("SortingArrays: "+ intArray[i]);
+        }
     }
 }
